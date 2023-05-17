fa35a7d8cb03842ef226ced5e913bbf69b651b1f
KENTA-MITANI <mitani.kenta88@gmail.com>
2023-05-18 08:36:48 +0900

    P20 リスト1-21〜25 configファイル新規作成＆使用

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -5,7 +5,12 @@
 </head>
 <body>
     <h1>Hello/Index</h1>
-    <p>{{ $msg }}</p>
+    <p>{!! $msg !!}</p>
+    <ul>
+        @foreach($data as $item)
+        <li>{!! $item !!}</li>
+        @endforeach
+    </ul>
 </body>
 
 </html>
